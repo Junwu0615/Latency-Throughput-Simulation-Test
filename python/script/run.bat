0bdc34c470f6cd25c77d9f4d75ea44ba1f7c7d39
jun.audis5 <jun.audis5@gmail.com>
2025-10-13 23:20:50 +0800

    update somethings

--- python/script/run.bat
+++ python/script/run.bat
@@ -16,16 +16,17 @@ REM 吞吐量 v2: 導入 ThreadPoolExecutor # 多執行緒
 @REM start cmd /k "python python\script\consumer_v2.py"
 
 REM 吞吐量 v3: 異步 I/O (Asyncio)
-start cmd /k "python python\script\consumer_v3.py"
+@REM start cmd /k "python python\script\consumer_v3.py"
 
 REM 吞吐量 v4: 水平擴展 [2]
 @REM start cmd /k "python python\script\consumer_v4.py"
 @REM start cmd /k "python python\script\consumer_v4.py"
 
 REM 吞吐量 v4: 水平擴展 [4]
-@REM start cmd /k "python python\script\consumer_v4.py"
-@REM start cmd /k "python python\script\consumer_v4.py"
-@REM start cmd /k "python python\script\consumer_v4.py"
+start cmd /k "python python\script\consumer_v4.py"
+start cmd /k "python python\script\consumer_v4.py"
+start cmd /k "python python\script\consumer_v4.py"
+start cmd /k "python python\script\consumer_v4.py"
 
 
 echo All Services [2] Are Running...
