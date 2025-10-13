e7864731d82d21c9ee5aba78c4060908f3af5a1c
jun.audis5 <jun.audis5@gmail.com>
2025-10-13 23:11:46 +0800

    update somethings

--- python/script/run.bat
+++ python/script/run.bat
@@ -8,10 +8,21 @@ call .\.venv\Scripts\activate
 echo Change venv: .\.venv\Scripts\activate
 
 start cmd /k "python python\script\producer.py"
-start cmd /k "python python\script\consumer_v1.py"
+
+REM 吞吐量 v1: 用 batch 方式塞資料 但還是需要依序等待 I/O
+@REM start cmd /k "python python\script\consumer_v1.py"
+
+REM 吞吐量 v2: 導入 ThreadPoolExecutor # 多執行緒
 @REM start cmd /k "python python\script\consumer_v2.py"
-@REM start cmd /k "python python\script\consumer_v3.py"
+
+REM 吞吐量 v3: 異步 I/O (Asyncio)
+start cmd /k "python python\script\consumer_v3.py"
+
+REM 吞吐量 v4: 水平擴展
 @REM start cmd /k "python python\script\consumer_v4.py"
+@REM start cmd /k "python python\script\consumer_v4.py"
+
+
 echo All Services [2] Are Running...
 
 pause
