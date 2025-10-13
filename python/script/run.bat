8949a8117bc0ff2e32b3cd92c6991498465fea73
jun.audis5 <jun.audis5@gmail.com>
2025-10-13 22:46:01 +0800

    update somethings

--- python/script/run.bat
+++ python/script/run.bat
@@ -8,8 +8,8 @@ call .\.venv\Scripts\activate
 echo Change venv: .\.venv\Scripts\activate
 
 start cmd /k "python python\script\producer.py"
-@REM start cmd /k "python python\script\consumer_v1.py"
-start cmd /k "python python\script\consumer_v2.py"
+start cmd /k "python python\script\consumer_v1.py"
+@REM start cmd /k "python python\script\consumer_v2.py"
 @REM start cmd /k "python python\script\consumer_v3.py"
 @REM start cmd /k "python python\script\consumer_v4.py"
 echo All Services [2] Are Running...
